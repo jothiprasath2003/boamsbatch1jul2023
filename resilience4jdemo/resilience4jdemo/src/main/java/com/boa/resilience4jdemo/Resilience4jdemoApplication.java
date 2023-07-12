b0c6af15a85a1014c46022b64f58af67beb34d7e
eswaribala <parameswaribala@gmail.com>
2023-07-12 16:01:04 +0530

    updated

---
+++ resilience4jdemo/resilience4jdemo/src/main/java/com/boa/resilience4jdemo/Resilience4jdemoApplication.java
@@ -0,0 +1,13 @@
+package com.boa.resilience4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jdemoApplication.class, args);
+	}
+
+}
---
+++ resilience4jdemo/resilience4jdemo/src/test/java/com/boa/resilience4jdemo/Resilience4jdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boa.resilience4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
