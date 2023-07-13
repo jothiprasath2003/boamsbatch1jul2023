5d5b60a336efa2a714bc8037d623b4db611687bf
eswaribala <parameswaribala@gmail.com>
2023-07-13 10:54:09 +0530

    updated

--- jwtsecurity/src/main/java/com/boa/jwtsecurity/models/Role.java
+++ jwtsecurity/src/main/java/com/boa/jwtsecurity/models/Role.java
@@ -10,8 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name="Role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
 
     @Id
