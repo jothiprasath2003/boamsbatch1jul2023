9da301a70d399cfc964abb7b134b99309170d109
eswaribala <parameswaribala@gmail.com>
2023-07-13 10:37:11 +0530

    updated

---
+++ jwtsecurity/src/main/java/com/boa/jwtsecurity/configurations/VaultConfiguration.java
@@ -0,0 +1,19 @@
+package com.boa.jwtsecurity.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ConfigurationProperties
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaultConfiguration {
+
+	private String username;
+	private String password;
+	
+}
